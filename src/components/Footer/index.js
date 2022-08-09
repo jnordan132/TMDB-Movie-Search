5acd629e7f5d034b1ae2fdc887923f4bc6d66a6c
jacob nordan <jacob.nordan113322@gmail.com>
2022-08-09 17:58:02 -0400

    added footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,19 @@
+function Footer() {
+  return (
+    <div className="footer">
+      <div>
+        <h6>
+          MovieWiki Was Created Using The{" "}
+          <a
+            target="_blank"
+            href="http://https://www.themoviedb.org/documentation/api"
+          >
+            TMDB API
+          </a>
+        </h6>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
