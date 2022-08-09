52ab0ff386a3057f9e8559a48076412fc965a639
jacob nordan <jacob.nordan113322@gmail.com>
2022-08-09 17:59:40 -0400

    added components to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
+import MovieContent from "./components/MovieContent";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <SearchBar />
+      <MovieContent />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
