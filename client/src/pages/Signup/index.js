b012667849a4e0f5583909e44798f9e9ef12599a
jacob nordan <jacob.nordan113322@gmail.com>
2023-01-28 16:51:57 -0500

    fixing typos, signup mutation broken with E11000 Dup key error now

--- client/src/pages/Signup/index.js
+++ client/src/pages/Signup/index.js
@@ -32,6 +32,7 @@ const Signup = () => {
       const { data } = await addUser({
         variables: { ...userFormData },
       });
+      console.log(data);
       Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
--- server/models/Movie.js
+++ server/models/Movie.js
@@ -1,11 +1,10 @@
 const { Schema } = require("mongoose");
 
-// This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
 const movieSchema = new Schema(
   {
     id: {
       type: Number,
-      // unique: true,
+      unique: true,
     },
     overview: {
       type: String,
