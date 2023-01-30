aab8f916114d59c2f5dcc96e8739a1bb3e3ede61
jacob nordan <jacob.nordan113322@gmail.com>
2023-01-29 20:55:03 -0500

    v9

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import Footer from "./components/Footer";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:3001/graphql",
+  uri: "/graphql",
   cache: new InMemoryCache(),
 });
 
