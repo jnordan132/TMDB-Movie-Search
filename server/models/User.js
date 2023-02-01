ee0c94eefb081eaf7e2c0581a17db035c92a85f3
jacob nordan <jacob.nordan113322@gmail.com>
2023-02-01 14:28:45 -0500

    idk

--- server/models/User.js
+++ server/models/User.js
@@ -96,7 +96,6 @@ const userSchema = new Schema(
   }
 );
 
-// hash user password
 userSchema.pre("save", async function (next) {
   if (this.isNew || this.isModified("password")) {
     const saltRounds = 10;
